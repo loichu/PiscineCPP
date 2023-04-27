93442c3f0cc3184bf488cfc91c642354a0327682
Loic Humbert <loichumbert5450@gmail.com>
2023-04-27 10:04:20 +0200

    CPP01 done

--- 01/ex06/Harl.cpp
+++ 01/ex06/Harl.cpp
@@ -1,9 +1,5 @@
 #include "Harl.hpp"
 
-Harl::Harl(std::string level)
-{
-}
-
 void	Harl::debug(void)
 {
 	std::cout << "DEBUG message" << std::endl;
@@ -26,21 +22,42 @@ void	Harl::error(void)
 
 void	Harl::complain(std::string level)
 {
+	int	log_level = -1;
+
 	std::string	levels[4] = {
 		"DEBUG",
 		"INFO",
 		"WARNING",
 		"ERROR"
 	};
-	void	(Harl::*f[4])(void) = {
-		&Harl::debug,
-		&Harl::info,
-		&Harl::warning,
-		&Harl::error
-	};
 	for (size_t i = 0; i < 4; ++i)
 	{
 		if (levels[i] == level)
-			(this->*f[i])();
+		{
+			log_level = i;
+			break;
+		}
+	}
+	switch (log_level)
+	{
+		case 0:
+			std::cout << "[ DEBUG ]" << std::endl;
+			debug();
+			std::cout << std::endl;
+		case 1:
+			std::cout << "[ INFO ]" << std::endl;
+			info();
+			std::cout << std::endl;
+		case 2:
+			std::cout << "[ WARNING ]" << std::endl;
+			warning();
+			std::cout << std::endl;
+		case 3:
+			std::cout << "[ ERROR ]" << std::endl;
+			error();
+			std::cout << std::endl;
+			break;
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
 	}
 }
--- 01/ex06/main.cpp
+++ 01/ex06/main.cpp
@@ -9,48 +9,6 @@ int main(int argc, char **argv)
 		std::cerr << "Expected exactly 1 argument, given " << argc - 1 << std::endl;
 		return (1);
 	}
-
-	std::string	levels[4] = {
-		"DEBUG",
-		"INFO",
-		"WARNING",
-		"ERROR"
-	};
-	int	level = -1;
-	for (size_t i = 0; i < 4; ++i)
-	{
-		if (levels[i] == argv[1])
-		{
-			level = i;
-			break;
-		}
-	}
-	if (i > 0)
-	{
-		std::cout << "[ " << levels[level] << " ]" << std::endl;
-		harl.complain(levels[level]);
-	}
-
-	/*
-	 *	As the switch statement only accepts  integers, this idea is dumb IMO
-	 */
-	//switch (level)
-	//{
-	//	case 0:
-	//		std::cout << "[ " << levels[0] << " ]" << std::endl;
-	//		harl.complain(levels[0]);
-	//	case 1:
-	//		std::cout << "[ INFO ]" << std::endl;
-	//		harl.complain("INFO");
-	//	case "WARNING":
-	//		std::cout << "[ WARNING ]" << std::endl;
-	//		harl.complain("WARNING");
-	//	case "ERROR":
-	//		std::cout << "[ ERROR ]" << std::endl;
-	//		harl.complain("ERROR");
-	//		break;
-	//	default:
-	//		std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
-	//}
+	harl.complain(argv[1]);
 	return 0;
 }
