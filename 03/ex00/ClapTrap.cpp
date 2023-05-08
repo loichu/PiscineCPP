57edd3616700e8b41d25607ed3a94406a0c37796
Loïc Humbert <loic@humbert-web.dev>
2023-05-08 22:03:10 +0200

    ex00 ok

--- 03/ex00/ClapTrap.cpp
+++ 03/ex00/ClapTrap.cpp
@@ -38,6 +38,45 @@ void	ClapTrap::attack(const std::string & target)
 		return;
 	}
 	std::cout << "ClapTrap " << _name << " attacks " << target
-			  << ", causing " << _attack << " points of damage!"
+			  << ", causing " << _attack << " points of damage! "
+			  << --_energy << " energy points left"
+			  << std::endl;
+}
+
+void	ClapTrap::takeDamage(unsigned int amount)
+{
+	if (_hit == 0)
+	{
+		std::cout << "ClapTrap " << _name << " is already dead" << std::endl;
+		return;
+	}
+	std::cout << "ClapTrap " << _name
+			  << " has been attacked, losing " 
+			  << amount << " hit points! ";
+	if (amount >= _hit)
+	{
+		_hit = 0;
+		std::cout << "ClapTrap " << _name << " died" << std::endl;
+	}
+	else
+	{
+		_hit -= amount;
+		std::cout << _hit << " hit points left" << std::endl;
+	}
+}
+
+void	ClapTrap::beRepaired(unsigned int amount)
+{
+	if (_hit == 0 || _energy == 0)
+	{
+		std::cout << "ClapTrap " << _name << " cannot be repaired" << std::endl;
+		return;
+	}
+	_hit += amount;
+	std::cout << "ClapTrap " << _name
+			  << " has been repaired, healing "
+			  << amount << " hit points! "
+			  << --_energy << " energy points left. "
+			  << _hit << " hit points left."
 			  << std::endl;
 }
--- 03/ex00/main.cpp
+++ 03/ex00/main.cpp
@@ -2,7 +2,32 @@
 
 int main(void)
 {
-	ClapTrap clap1("Thony");
-	ClapTrap clap2 = clap1;
-	ClapTrap clap3(clap2);
+	ClapTrap clap1("Rico l'asticot");
+	ClapTrap clap2;
+	ClapTrap clap3(clap1);
+	ClapTrap clap4(clap2);
+
+	clap1.attack("the cops");
+	clap2.attack("the gouvernement");
+	clap3.attack("Bill Gates");
+	clap4.attack("Nestlé");
+	clap1.attack("Vladimir Putin");
+	clap1.attack("himself");
+	clap1.takeDamage(0);
+	clap1.attack("an aligator");
+	clap1.takeDamage(5);
+	clap1.beRepaired(5);
+	clap1.takeDamage(9);
+	clap1.beRepaired(5);
+	clap1.takeDamage(3);
+	clap1.beRepaired(3);
+	clap1.beRepaired(3);
+	clap1.attack("an Apple fanboy");
+	clap1.takeDamage(10);
+	clap1.takeDamage(10);
+	clap1.beRepaired(3);
+	clap1.attack("a random guy");
+	clap2.attack("the gouvernement");
+	clap3.attack("Bill Gates");
+	clap4.attack("Nestlé");
 }
